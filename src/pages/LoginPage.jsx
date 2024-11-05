6a58bd734e343f3213c14dbca409ea54ea69610e
kim minseo <rlamintj@naver.com>
2024-11-06 03:57:03 +0900

    week_5 실습2 complete

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,7 +8,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 const schema = yup.object().shape({
     email: yup.string().email('올바른 이메일 형식이 아닙니다. 다시 확인해주세요!').required('이메일을 입력해주세요.'),
     password: yup.string()
-        .min(8, '비밀번호는 8자 이상이어야 합니다.')
+        .min(8, '비밀번호는 8자 이상이어야 합니다.') //에러문구
         .max(16, '비밀번호는 16자 이하여야 합니다.')
         .required('비밀번호를 입력해주세요.'),
 });
